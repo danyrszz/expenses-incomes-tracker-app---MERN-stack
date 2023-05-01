6f8aaf404ba3250b890654887426c8b6df62e775
Daniel Ramírez <daniel_ramirezs@hotmail.com>
2023-05-01 12:09:44 -0600

    added loading screen to login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,17 +1,22 @@
 import useAuth from "../utils/useAuth"
+import Loading from "../components/Loading"
 import "./Login.css"
 
 export default function Login(){
   
-  const {login} = useAuth()
+  const {login, loggingIn} = useAuth()
   return (
-    <div className="login-wrapper flex-centered">
-      <div className="card login-page flex-centered">
-        <form onSubmit={ e => login(e) } className="login-form">
-          <input type="password" name="" id="" />
-          <input type="submit" value="Iniciar Sesion"/>
-        </form>
+    <>
+      {loggingIn && <Loading></Loading>}
+      <div className="login-wrapper flex-centered">
+        <div className="card login-page flex-centered">
+          <form onSubmit={ e => login(e) } className="login-form">
+            <input type="password" name="" id="" />
+            <input type="submit" value="Iniciar Sesion"/>
+          </form>
+        </div>
       </div>
-    </div>
+
+    </>
   )
 }
--- frontend/src/utils/useAuth.jsx
+++ frontend/src/utils/useAuth.jsx
@@ -1,13 +1,16 @@
 import { saveData as fetch } from "./fetch";
 import { useNavigate } from "react-router-dom";
 import { endpoints } from "./endpoints";
+import { useState } from "react";
 
 export default function useAuth (){
 
   const navigate = useNavigate()
+  const [loggingIn, setLoggingIn] = useState(false)
 
   async function login(e){
     e.preventDefault()
+    setLoggingIn(true)
     const password = e.target[0].value
     if(!password) return alert("Escribe una contraseña.")
     const response = await fetch(
@@ -19,14 +22,16 @@ export default function useAuth (){
       localStorage.setItem('token', response.token)
       navigate("/")
     }else{
+      setLoggingIn(false)
       return alert("La constraseña es incorrecta.")
     }
   }
 
   function logout (){
     localStorage.removeItem('token')
+    setLoggingIn(false)
     navigate('/login')
   }
 
-  return {login, logout}
+  return {login, logout, loggingIn}
 }
